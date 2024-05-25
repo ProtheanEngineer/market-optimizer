76eef496531bee55b1949925b943f20a56aa426f
Raul Ramirez <raulramirezdominguez1998@gmail.com>
2024-05-25 17:13:34 +0200

    final

--- app/mercadona/productos/page.tsx
+++ app/mercadona/productos/page.tsx
@@ -1,55 +1,55 @@
 import Navbar from "@/components/navbar";
-import {Heading} from "@/components/ui/heading"
+import { Heading } from "@/components/ui/heading";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion" 
-import {Button} from "@/components/ui/button"
-import {db} from "@/lib/db"
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-
-const ProductsPage = async() => {
-  
-  const productos = await db.mercadona_Products.findMany({
-  })
+const ProductsPage = async () => {
+  const productos = await db.mercadona_Products.findMany({});
   const categorias = await db.mercadona_Categories.findMany({
     orderBy: {
       name: "asc",
-    }
-  })
+    },
+  });
   const subcategorias = await db.mercadona_Subcategories.findMany({
     orderBy: {
       name: "asc",
-    }
-  })
-  const agrupaciones = await db.mercadona_Product_Agrupations.findMany({})
-  
+    },
+  });
+  const agrupaciones = await db.mercadona_Product_Agrupations.findMany({});
+
   return (
     <div>
-      <Navbar></Navbar>
-      <Heading 
-      title="Productos" 
-      description="¡Echa un vistazo a todos los productos de Mercadona!"
+      <Navbar />
+      <Heading
+        title="Productos"
+        description="¡Echa un vistazo a todos los productos de Mercadona!"
       />
       <Accordion type="single" collapsible className="w-full">
-      {categorias.map((categoria) => (
-        <AccordionItem key={categoria.id} value={categoria.name}>
-          <AccordionTrigger>{categoria.name}</AccordionTrigger>
-          <AccordionContent>
-            {subcategorias
-              .filter((subcategoria) => subcategoria.id_category === categoria.id)
-              .map((filteredSubcategoria) => (
-                <div>
-                  <Button variant="link" key={filteredSubcategoria.id}>{filteredSubcategoria.name}</Button>  
-                </div>            ))}
-          </AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
+        {categorias.map((categoria) => (
+          <AccordionItem key={categoria.id} value={categoria.name}>
+            <AccordionTrigger>{categoria.name}</AccordionTrigger>
+            <AccordionContent>
+              {subcategorias
+                .filter(
+                  (subcategoria) => subcategoria.id_category === categoria.id
+                )
+                .map((filteredSubcategoria) => (
+                  <div key={filteredSubcategoria.id}>
+                    <Button variant="link">{filteredSubcategoria.name}</Button>
+                  </div>
+                ))}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
